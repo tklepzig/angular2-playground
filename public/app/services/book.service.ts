e64b19ebc0a82117e5fdd909d6f3b6ca43a6f98a
Thomas Klepzig <thomas.klepzig@comparex.de>
2016-09-21 10:14:57 +0200

    bugfix (typo)

--- public/app/services/book.service.ts
+++ public/app/services/book.service.ts
@@ -5,12 +5,9 @@ import { BookRepository } from './book.repository';
 @Injectable()
 export class BookService {
     constructor(private bookRepository: BookRepository) {
-    getBooks(): Book[] {
-        let books: Book[] = [];
-        books.push(<Book>{ name: "Book1" });
-        books.push(<Book>{ name: "Book2" });
-        books.push(<Book>{ name: "Book3" });
+    }
 
-        return books;
+    getBooks(): Book[] {
+        return this.bookRepository.getBooks();
     }
 }
