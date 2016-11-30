3663b3627716fc9da61e997e1245b1f811e85288
Thomas Klepzig <thomas.klepzig@comparex.de>
2016-11-30 15:58:21 +0100

    successfully edited tests using mock framework

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -2,13 +2,25 @@
 
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { BookListComponent } from './bookList/bookList.component';
+import { Book } from './models/book';
+import { BookRepository } from './services/book.repository';
+import * as mockito from 'ts-mockito';
 
 describe('AppComponent', () => {
   beforeEach(() => {
+
+    var mockBookRepository = mockito.mock(BookRepository);
+    mockito.when(mockBookRepository.getBooks()).thenReturn(new Promise<Book[]>((resolve) => resolve(new Array<Book>())));
+
     TestBed.configureTestingModule({
       declarations: [
-        AppComponent
+        AppComponent,
+        BookListComponent
       ],
+      providers: [
+        { provide: BookRepository, useValue: mockito.instance(mockBookRepository) }
+      ]
     });
   });
 
@@ -18,16 +30,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   }));
 
-  it(`should have as title 'app works!'`, async(() => {
-    let fixture = TestBed.createComponent(AppComponent);
-    let app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app works!');
-  }));
-
   it('should render title in a h1 tag', async(() => {
     let fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     let compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('app works!');
+    expect(compiled.querySelector('h1').textContent).toContain('Books');
   }));
 });
